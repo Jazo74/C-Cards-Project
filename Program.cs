6f6e11485b6337906b466610fc0e805009125ef2
Jazo74 <55430157+Jazo74@users.noreply.github.com>
2020-02-17 12:59:09 +0100

    first commit

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Cardgame
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
