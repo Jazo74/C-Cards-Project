8c06701e860bfacae0a121b7f830159f4acac4ad
stvnSzabo <55262494+stvnSzabo@users.noreply.github.com>
2020-02-19 10:28:11 +0100

    Add files via upload

---
+++ Dealer.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+
+namespace Cardgame
+{
+    class Menu
+
+
+    {
+        public void showMenu(string[] menupoints)
+        {
+            int index = 1;
+            foreach (string point in menupoints)
+            {
+                Console.WriteLine("(" + index + ") " + point);
+                index++;
+            }
+            Console.WriteLine("(0) Exit Program");
+        }
+        public void start()
+        {
+            string[] menupoints = new string[]
+            {
+                "Egy",
+                "Kettő"
+
+            };
+
+
+            while (true)
+            {
+                showMenu(menupoints);
+                Console.WriteLine("Type in your choice: ");
+                int choice = int.Parse(Console.ReadLine());
+                switch (choice)
+                {
+                    case 0:
+                        {
+                            Environment.Exit(0);
+                            break;
+                        }
+
+
+                }
+            }
+        }
+    }
+}
